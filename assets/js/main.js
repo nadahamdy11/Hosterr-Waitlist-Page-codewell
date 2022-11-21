572a2aeee4713f955bc9435aa77d23538c02eeb0
unknown <nh998449gmail.com>
2022-11-21 21:28:43 +0200

    first

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+jQuery("#responsive_headline").fitText(1.2, { minFontSize: '20px', maxFontSize: '40px' });
+wow = new WOW(
+  {
+  boxClass:     'wow',      // default
+  animateClass: 'animated', // default
+  offset:       0,          // default
+  mobile:       true,       // default
+  live:         true        // default
+}
+)
+wow.init();
